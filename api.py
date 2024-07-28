3c87e471f9f61f074a4e59d96c6685f65efa6ade
MuhamadAlee <alliranjha807@gmail.com>
2024-07-28 19:08:45 +0500

    CORS fix

--- api.py
+++ api.py
@@ -18,9 +18,15 @@ load_dotenv()
 app = FastAPI()
 
 
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    # Add other origins as needed
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
