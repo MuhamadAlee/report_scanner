f7b0d0fde896b9f1e694e13071dd874dbee485d4
MuhamadAlee <alliranjha807@gmail.com>
2024-12-10 22:30:24 +0500

    login-fixes

--- routes/auth.py
+++ routes/auth.py
@@ -6,26 +6,22 @@ from config.database import get_db
 from models.user import User
 from fastapi.security import OAuth2PasswordRequestForm
 from controllers.auth import create_access_token, revoke_token, get_current_user, get_current_token
-from pydantic import BaseModel
-class OAuth2LoginRequest(BaseModel):
-    grant_type: str = "password"  # Default to "password"
-    username: str
-    password: str
+
+
 
 auth = APIRouter(tags=['authentication'])
 
 
 @auth.post("/login/", response_model=Token)
-def login(userdetails: OAuth2LoginRequest = Depends(), db: Session = Depends(get_db)):
-    # Extract username and password from the request body
-    user = db.query(User).filter(User.email == userdetails.username).first()
+def login(userdetails: OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
     
+    user = db.query(User).filter(User.email == userdetails.username).first()
+
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User does not exist')
     
     if user.hashed_password != hash_password(userdetails.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Wrong Password')
-    
     if not user.is_active:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Disabled User')
     
