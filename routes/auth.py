da50f9ab11783d6c4b12a3d527e985aa629f44c3
MuhamadAlee <alliranjha807@gmail.com>
2024-12-10 22:36:54 +0500

    login-fix

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,5 @@
 from utils.util import hash_password
-from fastapi import Form, APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from schemas.auth import Token
 from config.database import get_db
@@ -11,29 +11,19 @@ from controllers.auth import create_access_token, revoke_token, get_current_user
 
 auth = APIRouter(tags=['authentication'])
 
-@auth.post("/login/", response_model=Token)
-def login(
-    userdetails: OAuth2PasswordRequestForm = Depends(), 
-    db: Session = Depends(get_db),
-    grant_type: str = Form("password")  # Default to "password" if it's not provided
-):
-    # Ensure that grant_type is "password"
-    if grant_type != "password":
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Invalid grant type"
-        )
 
+@auth.post("/login/", response_model=Token)
+def login(userdetails: OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
+    
     user = db.query(User).filter(User.email == userdetails.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User does not exist")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User does not exist')
     
     if user.hashed_password != hash_password(userdetails.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong Password")
-    
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Wrong Password')
     if not user.is_active:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Disabled User")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Disabled User')
     
     return create_access_token(data=user)
 
