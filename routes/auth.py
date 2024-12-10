68514eb5ba91f99e0103e5f3944624cadae86e1b
MuhamadAlee <alliranjha807@gmail.com>
2024-12-10 22:13:49 +0500

    login-fixes

--- routes/auth.py
+++ routes/auth.py
@@ -7,22 +7,24 @@ from models.user import User
 from fastapi.security import OAuth2PasswordRequestForm
 from controllers.auth import create_access_token, revoke_token, get_current_user, get_current_token
 
-
+class LoginRequest(BaseModel):
+    username: str
+    password: str
 
 auth = APIRouter(tags=['authentication'])
 
 
 @auth.post("/login/", response_model=Token)
-def login(userdetails: OAuth2PasswordRequestForm = Depends(), db:Session = Depends(get_db)):
-    
+def login(userdetails: LoginRequest, db: Session = Depends(get_db)):
+    # Extract username and password from the request body
     user = db.query(User).filter(User.email == userdetails.username).first()
-    print("---------------------------")
-
+    
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='User does not exist')
     
     if user.hashed_password != hash_password(userdetails.password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Wrong Password')
+    
     if not user.is_active:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Disabled User')
     
